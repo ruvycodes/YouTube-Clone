d8a16e3d6f6b202b5c2731f4e97ec9435c15e7d5
Dhruv <153931946+ruvycodes@users.noreply.github.com>
2024-03-27 00:11:14 +0530

    Update constants.jsx

--- src/Utils/constants.jsx
+++ src/Utils/constants.jsx
@@ -1,2 +1,2 @@
-export const YOUTUBE_API_Key = "AIzaSyCRbk95aIpMSrgCVEQJ--enAMwvGDtZPXg"
+export const YOUTUBE_API_Key = "YOUR API KEY"
 export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=statistics%20%2C%20contentDetails%20%2C%20snippet&chart=mostPopular&key=" + YOUTUBE_API_Key
